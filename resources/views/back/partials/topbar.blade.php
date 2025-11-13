829ef91d78a24ed830ca200f988181f0573dc197
Mamadou Bhoye Bah <bhoyemad11@gmail.com>
2025-11-13 15:35:27 +0000

    test

--- resources/views/back/partials/topbar.blade.php
+++ resources/views/back/partials/topbar.blade.php
@@ -215,7 +215,7 @@
         </li>
 
         <!-- Nav Item - User Information -->
-        <li class="nav-item dropdown no-arrow">
+        {{-- <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
@@ -226,7 +226,7 @@
                 <a class="dropdown-item" href="{{ route('profile.edit') }}">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Profile
-                </a>
+                </a> --}}
 
                 <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
